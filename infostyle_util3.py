8ecfc7f6c2940317be7cd927b41763087da2d292
sdujump <sdujump@gmail.com>
2016-12-01 17:58:56 +0800

    111

--- infostyle_util3.py
+++ infostyle_util3.py
@@ -13,81 +13,84 @@ import tensorflow.contrib.slim as slim
 import vgg
 
 
-def generator(z):
+def generator(z, reuse=False):
+    with tf.variable_scope('generator', reuse=reuse):
 
-    initializer = tf.truncated_normal_initializer(stddev=0.02)
+        initializer = tf.truncated_normal_initializer(stddev=0.02)
 
-    zP = slim.fully_connected(z, 4 * 4 * 256, normalizer_fn=slim.batch_norm,
-                              activation_fn=tf.nn.relu, scope='g_project', weights_initializer=initializer)
-    zCon = tf.reshape(zP, [-1, 4, 4, 256])
+        zP = slim.fully_connected(z, 4 * 4 * 256, normalizer_fn=slim.batch_norm,
+                                  activation_fn=tf.nn.relu, scope='g_project', weights_initializer=initializer)
+        zCon = tf.reshape(zP, [-1, 4, 4, 256])
 
-    gen1 = slim.convolution2d(
-        zCon, num_outputs=128, kernel_size=[3, 3],
-        padding="SAME", normalizer_fn=slim.batch_norm,
-        activation_fn=tf.nn.relu, scope='g_conv1', weights_initializer=initializer)
-    gen1 = tf.depth_to_space(gen1, 2)
+        gen1 = slim.convolution2d(
+            zCon, num_outputs=128, kernel_size=[3, 3],
+            padding="SAME", normalizer_fn=slim.batch_norm,
+            activation_fn=tf.nn.relu, scope='g_conv1', weights_initializer=initializer)
+        gen1 = tf.depth_to_space(gen1, 2)
 
-    gen2 = slim.convolution2d(
-        gen1, num_outputs=64, kernel_size=[3, 3],
-        padding="SAME", normalizer_fn=slim.batch_norm,
-        activation_fn=tf.nn.relu, scope='g_conv2', weights_initializer=initializer)
-    gen2 = tf.depth_to_space(gen2, 2)
+        gen2 = slim.convolution2d(
+            gen1, num_outputs=64, kernel_size=[3, 3],
+            padding="SAME", normalizer_fn=slim.batch_norm,
+            activation_fn=tf.nn.relu, scope='g_conv2', weights_initializer=initializer)
+        gen2 = tf.depth_to_space(gen2, 2)
 
-    gen3 = slim.convolution2d(
-        gen2, num_outputs=32, kernel_size=[3, 3],
-        padding="SAME", normalizer_fn=slim.batch_norm,
-        activation_fn=tf.nn.relu, scope='g_conv3', weights_initializer=initializer)
-    gen3 = tf.depth_to_space(gen3, 2)
+        gen3 = slim.convolution2d(
+            gen2, num_outputs=32, kernel_size=[3, 3],
+            padding="SAME", normalizer_fn=slim.batch_norm,
+            activation_fn=tf.nn.relu, scope='g_conv3', weights_initializer=initializer)
+        gen3 = tf.depth_to_space(gen3, 2)
 
-    g_out = slim.convolution2d(
-        gen3, num_outputs=3, kernel_size=[32, 32], padding="SAME",
-        biases_initializer=None, activation_fn=tf.nn.tanh,
-        scope='g_out', weights_initializer=initializer)
+        g_out = slim.convolution2d(
+            gen3, num_outputs=3, kernel_size=[32, 32], padding="SAME",
+            biases_initializer=None, activation_fn=tf.nn.tanh,
+            scope='g_out', weights_initializer=initializer)
 
-    return g_out
+        return g_out
 
 
 def discriminator(bottom, cat_list, conts, reuse=False):
-    initializer = tf.truncated_normal_initializer(stddev=0.02)
+    with tf.variable_scope('discriminator', reuse=reuse):
 
-    dis1 = slim.convolution2d(bottom, 32, [3, 3], padding="SAME",
-                              biases_initializer=None, activation_fn=lrelu,
-                              reuse=reuse, scope='d_conv1', weights_initializer=initializer)
-    dis1 = tf.space_to_depth(dis1, 2)
+        initializer = tf.truncated_normal_initializer(stddev=0.02)
 
-    dis2 = slim.convolution2d(dis1, 64, [3, 3], padding="SAME",
-                              normalizer_fn=slim.batch_norm, activation_fn=lrelu,
-                              reuse=reuse, scope='d_conv2', weights_initializer=initializer)
-    dis2 = tf.space_to_depth(dis2, 2)
+        dis1 = slim.convolution2d(bottom, 32, [3, 3], padding="SAME",
+                                  biases_initializer=None, activation_fn=lrelu,
+                                  reuse=reuse, scope='d_conv1', weights_initializer=initializer)
+        dis1 = tf.space_to_depth(dis1, 2)
 
-    dis3 = slim.convolution2d(dis2, 128, [3, 3], padding="SAME",
-                              normalizer_fn=slim.batch_norm, activation_fn=lrelu,
-                              reuse=reuse, scope='d_conv3', weights_initializer=initializer)
-    dis3 = tf.space_to_depth(dis3, 2)
+        dis2 = slim.convolution2d(dis1, 64, [3, 3], padding="SAME",
+                                  normalizer_fn=slim.batch_norm, activation_fn=lrelu,
+                                  reuse=reuse, scope='d_conv2', weights_initializer=initializer)
+        dis2 = tf.space_to_depth(dis2, 2)
 
-    dis4 = slim.fully_connected(slim.flatten(dis3), 1024, activation_fn=lrelu,
-                                reuse=reuse, scope='d_fc1', weights_initializer=initializer)
+        dis3 = slim.convolution2d(dis2, 128, [3, 3], padding="SAME",
+                                  normalizer_fn=slim.batch_norm, activation_fn=lrelu,
+                                  reuse=reuse, scope='d_conv3', weights_initializer=initializer)
+        dis3 = tf.space_to_depth(dis3, 2)
 
-    d_out = slim.fully_connected(dis4, 1, activation_fn=tf.nn.sigmoid,
-                                 reuse=reuse, scope='d_out', weights_initializer=initializer)
+        dis4 = slim.fully_connected(slim.flatten(dis3), 1024, activation_fn=lrelu,
+                                    reuse=reuse, scope='d_fc1', weights_initializer=initializer)
 
-    q_a = slim.fully_connected(dis4, 128, normalizer_fn=slim.batch_norm,
-                               reuse=reuse, scope='q_fc1', weights_initializer=initializer)
+        d_out = slim.fully_connected(dis4, 1, activation_fn=tf.nn.sigmoid,
+                                     reuse=reuse, scope='d_out', weights_initializer=initializer)
 
-    # Here we define the unique layers used for the q-network. The number of outputs depends on the number of
-    # latent variables we choose to define.
-    q_cat_outs = []
-    for idx, var in enumerate(cat_list):
-        q_outA = slim.fully_connected(q_a, var, activation_fn=tf.nn.softmax,
-                                      reuse=reuse, scope='q_out_cat_' + str(idx), weights_initializer=initializer)
-        q_cat_outs.append(q_outA)
+        q_a = slim.fully_connected(dis4, 128, normalizer_fn=slim.batch_norm,
+                                   reuse=reuse, scope='q_fc1', weights_initializer=initializer)
 
-    q_cont_outs = None
-    if conts > 0:
-        q_cont_outs = slim.fully_connected(q_a, conts, activation_fn=tf.nn.tanh,
-                                           reuse=reuse, scope='q_out_cont_' + str(conts), weights_initializer=initializer)
+        # Here we define the unique layers used for the q-network. The number of outputs depends on the number of
+        # latent variables we choose to define.
+        q_cat_outs = []
+        for idx, var in enumerate(cat_list):
+            q_outA = slim.fully_connected(q_a, var, activation_fn=tf.nn.softmax,
+                                          reuse=reuse, scope='q_out_cat_' + str(idx), weights_initializer=initializer)
+            q_cat_outs.append(q_outA)
 
-    return d_out, q_cat_outs, q_cont_outs
+        q_cont_outs = None
+        if conts > 0:
+            q_cont_outs = slim.fully_connected(q_a, conts, activation_fn=tf.nn.tanh,
+                                               reuse=reuse, scope='q_out_cont_' + str(conts), weights_initializer=initializer)
+
+        return d_out, q_cat_outs, q_cont_outs
 
 
 def lrelu(x, leak=0.2, name="lrelu"):
