e1bafd5ac2fea5c27e38c0df2b0ee2be2ac653dd
sdujump <sdujump@gmail.com>
2016-12-02 02:00:21 +0800

    111

--- infostyle3.py
+++ infostyle3.py
@@ -43,30 +43,16 @@ z_size = 64  # Size of initial z vector used for generator.
 image_size = 32
 # Define latent variables.
 # Each entry in this list defines a categorical variable of a specific size.
-categorical_list = [10]
+categorical_list = 10
 number_continuous = 2  # The number of continous variables.
 
 # This initializaer is used to initialize all the weights of the network.
 initializer = tf.truncated_normal_initializer(stddev=0.02)
 
 # These placeholders are used for input into the generator and discriminator, respectively.
-z_in = tf.placeholder(shape=[None, z_size], dtype=tf.float32)  # Random vector
 real_in = tf.placeholder(shape=[None, image_size, image_size, 3], dtype=tf.float32)  # Real images
 
-# These placeholders load the latent variables.
-latent_cat_in = tf.placeholder(shape=[None, 1], dtype=tf.int32)
-# latent_cat_list = tf.split(1, 1, latent_cat_in)
-latent_cont_in = tf.placeholder(shape=[None, number_continuous], dtype=tf.float32)
-
-oh_list = []
-latent_oh = tf.one_hot(tf.reshape(latent_cat_in, [-1]), 10)
-oh_list.append(latent_oh)
-
-# Concatenate all c and z variables.
-z_lats = oh_list[:]
-z_lats.append(z_in)
-z_lats.append(latent_cont_in)
-z_lat = tf.concat(1, z_lats)
+z_lat = tf.placeholder(shape=[None, z_size + categorical_list + number_continuous], dtype=tf.float32)  # Random vector
 
 
 # The below code is responsible for applying gradient descent to update
@@ -84,9 +70,9 @@ for i in xrange(num_gpus):
         with tf.device('/gpu:%d' % i):
             with tf.name_scope('Tower_%d' % (i)) as scope:
 
-                Gz = infostyle_util.generator(z_lat[i * batch_size:(i + 1) * batch_size, :])  # Generates images from random z vectors
+                Gz = infostyle_util.generator(z_lat)  # Generates images from random z vectors
                 # Produces probabilities for real images
-                Dx, _, _ = infostyle_util.discriminator(real_in[i * batch_size:(i + 1) * batch_size, :, :, :])
+                Dx, _, _ = infostyle_util.discriminator(real_in)
                 # Produces probabilities for generator images
                 Dg, QgCat, QgCont = infostyle_util.discriminator(Gz, reuse=True)
 
@@ -97,15 +83,11 @@ for i in xrange(num_gpus):
 
                 # Combine losses for each of the categorical variables.
                 cat_losses = []
-                cat_loss = -tf.reduce_sum(oh_list[0][i * batch_size:(i + 1) * batch_size, :] * tf.log(QgCat[0]), reduction_indices=1)
+                cat_loss = -tf.reduce_sum(z_lat[:, 0:categorical_list] * tf.log(QgCat[0]), reduction_indices=1)
                 cat_losses.append(cat_loss)
 
                 # Combine losses for each of the continous variables.
-                if number_continuous > 0:
-                    q_cont_loss = tf.reduce_sum(
-                        0.5 * tf.square(latent_cont_in - QgCont), reduction_indices=1)
-                else:
-                    q_cont_loss = tf.constant(0.0)
+                q_cont_loss = tf.reduce_sum(0.5 * tf.square(z_lat[:, categorical_list + z_size:] - QgCont), reduction_indices=1)
 
                 q_cont_loss = tf.reduce_mean(q_cont_loss)
                 q_cat_loss = tf.reduce_mean(cat_losses)
@@ -144,7 +126,7 @@ def train_infogan():
     epoch = 0
     batch_size = 64  # Size of image batch to apply at each iteration.
     num_epochs = 50  # Total number of iterations to use.
-    total_batch = int(np.floor(num_examples / (batch_size * num_gpus)))
+    total_batch = int(np.floor(num_examples / batch_size))
     # Directory to save sample images from generator in.
     sample_directory = './figsTut'
     model_directory = './models'  # Directory to save trained model to.
@@ -155,39 +137,55 @@ def train_infogan():
     with tf.Session(config=config) as sess:
         sess.run(init)
         while epoch < num_epochs:
-            iter_ = infostyle_util.data_iterator(images, filenames, batch_size * num_gpus)
+            iter_ = infostyle_util.data_iterator(images, filenames, batch_size)
             for i in tqdm.tqdm(range(total_batch)):
                 # Generate a random z batch
-                zs = np.random.uniform(-1.0, 1.0, size=[batch_size * num_gpus, z_size]).astype(np.float32)
-                lcat = np.random.randint(0, 10, [batch_size * num_gpus, 1])  # Generate random c batch
-                lcont = np.random.uniform(-1, 1, [batch_size * num_gpus, number_continuous])
+                zs = np.random.uniform(-1.0, 1.0, size=[batch_size, z_size]).astype(np.float32)
+                lcat = np.random.randint(0, 10, [batch_size, 1])  # Generate random c batch
+                lcont = np.random.uniform(-1, 1, [batch_size, number_continuous])
+
+                oh_list = []
+                latent_oh = tf.one_hot(tf.reshape(lcat, [-1]), 10)
+                oh_list.append(latent_oh)
+
+                # Concatenate all c and z variables.
+                z_lats = oh_list[:]
+                z_lats.append(zs)
+                z_lats.append(lcont)
+                zlat = tf.concat(1, z_lats)
 
                 # Draw a sample batch from MNIST dataset.
                 xs, _ = iter_.next()
                 # xs, _ = mnist.train.next_batch(batch_size)
                 # Transform it to be between -1 and 1
-                xs = (np.reshape(xs, [batch_size * num_gpus, image_size, image_size, 3]) / 255.0 - 0.5) * 2.0
+                xs = (np.reshape(xs, [batch_size, image_size, image_size, 3]) / 255.0 - 0.5) * 2.0
                 # xs = np.lib.pad(xs, ((0, 0), (2, 2), (2, 2), (0, 0)), 'constant', constant_values=(-1, -1))  # Pad the images so the are 32x32
 
-                _, dLoss = sess.run([update_D, d_loss], feed_dict={z_in: zs, real_in: xs, latent_cat_in: lcat, latent_cont_in: lcont})  # Update the discriminator
+                _, dLoss = sess.run([update_D, d_loss], feed_dict={z_lat: zlat, real_in: xs})  # Update the discriminator
                 # Update the generator, twice for good measure.
-                _, gLoss = sess.run([update_G, g_loss], feed_dict={
-                                    z_in: zs, latent_cat_in: lcat, latent_cont_in: lcont})
-                _, qLoss, qK, qC = sess.run([update_Q, q_loss, q_cont_loss, q_cat_loss], feed_dict={
-                                            z_in: zs, latent_cat_in: lcat, latent_cont_in: lcont})  # Update to optimize mutual information.
+                _, gLoss = sess.run([update_G, g_loss], feed_dict={z_lat: zlat})
+                _, qLoss, qK, qC = sess.run([update_Q, q_loss, q_cont_loss, q_cat_loss], feed_dict={z_lat: zlat})  # Update to optimize mutual information.
                 if i % 100 == 0:
                     print "epoch: " + str(epoch) + " Gen Loss: " + str(gLoss) + " Disc Loss: " + str(dLoss) + " Q Losses: " + str([qK, qC])
                     # Generate another z batch
-                    z_sample = np.random.uniform(-1.0, 1.0,
-                                                 size=[100, z_size]).astype(np.float32)
-                    lcat_sample = np.reshape(np.array([e for e in range(10) for tempi in range(10)]), [100, 1])
-                    a = a = np.reshape(
-                        np.array([[(e / 4.5 - 1.)] for e in range(10) for tempj in range(10)]), [10, 10]).T
-                    b = np.reshape(a, [100, 1])
-                    c = np.zeros_like(b)
-                    lcont_sample = np.hstack([b, c])
+                    zs = np.random.uniform(-1.0, 1.0, size=[100, z_size]).astype(np.float32)
+                    lcat = np.reshape(np.array([e for e in range(10) for tempi in range(10)]), [100, 1])
+                    aa = np.reshape(np.array([[(e / 4.5 - 1.)] for e in range(10) for tempj in range(10)]), [10, 10]).T
+                    bb = np.reshape(aa, [100, 1])
+                    cc = np.zeros_like(bb)
+                    lcont = np.hstack([bb, cc])
+
+                    oh_list = []
+                    latent_oh = tf.one_hot(tf.reshape(lcat, [-1]), 10)
+                    oh_list.append(latent_oh)
+
+                    # Concatenate all c and z variables.
+                    z_lats = oh_list[:]
+                    z_lats.append(zs)
+                    z_lats.append(lcont)
+                    zlat = tf.concat(1, z_lats)
                     # Use new z to get sample images from generator.
-                    samples = sess.run(Gz, feed_dict={z_in: z_sample, latent_cat_in: lcat_sample, latent_cont_in: lcont_sample})
+                    samples = sess.run(Gz, feed_dict={z_lat: zlat})
                     if not os.path.exists(sample_directory):
                         os.makedirs(sample_directory)
                     # Save sample generator images for viewing training
@@ -215,17 +213,24 @@ def test_infogan():
         saver.restore(sess, ckpt.model_checkpoint_path)
 
         # Generate another z batch
-        z_sample = np.random.uniform(-1.0, 1.0,
-                                     size=[100, z_size]).astype(np.float32)
-        lcat_sample = np.reshape(
-            np.array([e for e in range(10) for _ in range(10)]), [100, 1])
-        a = a = np.reshape(np.array([[(e / 4.5 - 1.)] for e in range(10) for _ in range(10)]), [10, 10]).T
-        b = np.reshape(a, [100, 1])
-        c = np.zeros_like(b)
-        lcont_sample = np.hstack([b, c])
+        zs = np.random.uniform(-1.0, 1.0, size=[100, z_size]).astype(np.float32)
+        lcat = np.reshape(np.array([e for e in range(10) for tempi in range(10)]), [100, 1])
+        aa = np.reshape(np.array([[(e / 4.5 - 1.)] for e in range(10) for tempj in range(10)]), [10, 10]).T
+        bb = np.reshape(aa, [100, 1])
+        cc = np.zeros_like(bb)
+        lcont = np.hstack([bb, cc])
+
+        oh_list = []
+        latent_oh = tf.one_hot(tf.reshape(lcat, [-1]), 10)
+        oh_list.append(latent_oh)
+
+        # Concatenate all c and z variables.
+        z_lats = oh_list[:]
+        z_lats.append(zs)
+        z_lats.append(lcont)
+        zlat = tf.concat(1, z_lats)
         # Use new z to get sample images from generator.
-        samples = sess.run(Gz, feed_dict={
-                           z_in: z_sample, latent_cat_in: lcat_sample, latent_cont_in: lcont_sample})
+        samples = sess.run(Gz, feed_dict={z_lat: zlat})
         if not os.path.exists(sample_directory):
             os.makedirs(sample_directory)
         # Save sample generator images for viewing training progress.
