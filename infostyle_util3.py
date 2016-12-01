cca8c19e3e9f50a7c08d1831cb0d8106c7f7d5a5
sdujump <sdujump@gmail.com>
2016-12-01 20:27:15 +0800

    1111

--- infostyle_util3.py
+++ infostyle_util3.py
@@ -49,32 +49,10 @@ def generator(z, reuse=False):
 
         return g_out_concat
 
-
+'''
 def discriminator(bottom, cat_list, conts, reuse=False):
     with tf.variable_scope('discriminator', reuse=reuse):
-        '''
-        dis1 = slim.convolution2d(bottom, 128, [3, 3], padding="SAME", stride=2,
-                                  biases_initializer=None, activation_fn=lrelu,
-                                  reuse=reuse, scope='d_conv1')
 
-        dis2 = slim.convolution2d(dis1, 256, [3, 3], padding="SAME", stride=2,
-                                  normalizer_fn=slim.batch_norm, activation_fn=lrelu,
-                                  reuse=reuse, scope='d_conv2')
-
-        dis3 = slim.convolution2d(dis2, 512, [3, 3], padding="SAME", stride=2,
-                                  normalizer_fn=slim.batch_norm, activation_fn=lrelu,
-                                  reuse=reuse, scope='d_conv3')
-
-        dis4 = slim.fully_connected(slim.flatten(dis3), 1024, activation_fn=lrelu,
-                                    reuse=reuse, scope='d_fc1')
-
-        d_out = slim.fully_connected(dis4, 1, activation_fn=tf.nn.sigmoid,
-                                     reuse=reuse, scope='d_out')
-
-        q_a = slim.fully_connected(dis4, 128, normalizer_fn=slim.batch_norm,
-                                   reuse=reuse, scope='q_fc1')
-
-        '''
         initializer = tf.truncated_normal_initializer(stddev=0.02)
 
         dis1 = slim.convolution2d(bottom, 128, [3, 3], padding="SAME", stride=2,
@@ -112,6 +90,50 @@ def discriminator(bottom, cat_list, conts, reuse=False):
                                                reuse=reuse, scope='q_out_cont_' + str(conts), weights_initializer=initializer)
 
         return d_out, q_cat_outs, q_cont_outs
+'''
+
+
+def discriminator(bottom, cat_list, conts, reuse=False):
+    initializer = tf.truncated_normal_initializer(stddev=0.02)
+
+    dis1 = slim.convolution2d(bottom, 32, [3, 3], padding="SAME",
+                              biases_initializer=None, activation_fn=lrelu,
+                              reuse=reuse, scope='d_conv1', weights_initializer=initializer)
+    dis1 = tf.space_to_depth(dis1, 2)
+
+    dis2 = slim.convolution2d(dis1, 64, [3, 3], padding="SAME",
+                              normalizer_fn=slim.batch_norm, activation_fn=lrelu,
+                              reuse=reuse, scope='d_conv2', weights_initializer=initializer)
+    dis2 = tf.space_to_depth(dis2, 2)
+
+    dis3 = slim.convolution2d(dis2, 128, [3, 3], padding="SAME",
+                              normalizer_fn=slim.batch_norm, activation_fn=lrelu,
+                              reuse=reuse, scope='d_conv3', weights_initializer=initializer)
+    dis3 = tf.space_to_depth(dis3, 2)
+
+    dis4 = slim.fully_connected(slim.flatten(dis3), 1024, activation_fn=lrelu,
+                                reuse=reuse, scope='d_fc1', weights_initializer=initializer)
+
+    d_out = slim.fully_connected(dis4, 1, activation_fn=tf.nn.sigmoid,
+                                 reuse=reuse, scope='d_out', weights_initializer=initializer)
+
+    q_a = slim.fully_connected(dis4, 128, normalizer_fn=slim.batch_norm,
+                               reuse=reuse, scope='q_fc1', weights_initializer=initializer)
+
+    # Here we define the unique layers used for the q-network. The number of outputs depends on the number of
+    # latent variables we choose to define.
+    q_cat_outs = []
+    for idx, var in enumerate(cat_list):
+        q_outA = slim.fully_connected(q_a, var, activation_fn=tf.nn.softmax,
+                                      reuse=reuse, scope='q_out_cat_' + str(idx), weights_initializer=initializer)
+        q_cat_outs.append(q_outA)
+
+    q_cont_outs = None
+    if conts > 0:
+        q_cont_outs = slim.fully_connected(q_a, conts, activation_fn=tf.nn.tanh,
+                                           reuse=reuse, scope='q_out_cont_' + str(conts), weights_initializer=initializer)
+
+    return d_out, q_cat_outs, q_cont_outs
 
 
 def lrelu(x, leak=0.2, name="lrelu"):
