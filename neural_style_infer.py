92a40ac61c77946756f3e3f29294bfc63976502c
sdujump <sdujump@gmail.com>
2016-12-09 13:55:25 +0800

    a

--- neural_style_infer.py
+++ neural_style_infer.py
@@ -39,30 +39,30 @@ def data_iterator(images, filenames, batch_size):
 
 
 def inference(path, name, gpun):
+    with tf.Graph().as_default(), tf.device('/cpu:0'):
+        with h5py.File(''.join(['datasets/coco-256.h5']), 'r') as hf:
+            content_images = hf['images'].value
+            content_names = hf['filenames'].value
+        content_holder = tf.placeholder(shape=[1, 256, 256, 3], dtype=tf.float32)  # Random vector
+        total_batch = len(content_images)
+        content_iter = data_iterator(content_images, content_names, 1)
 
-    with h5py.File(''.join(['datasets/coco-256.h5']), 'r') as hf:
-        content_images = hf['images'].value
-        content_names = hf['filenames'].value
-    content_holder = tf.placeholder(shape=[1, 256, 256, 3], dtype=tf.float32)  # Random vector
-    total_batch = len(content_images)
-    content_iter = data_iterator(content_images, content_names, 1)
+        with tf.device('/gpu:%d' % gpun):
+            generated = model.net(content_holder)
+            output_format = tf.saturate_cast(generated + mean_pixel, tf.uint8)
 
-    with tf.device('/gpu:%d' % gpun):
-        generated = model.net(content_holder)
-        output_format = tf.saturate_cast(generated + mean_pixel, tf.uint8)
+            sess = tf.Session()
+            sess.run(tf.initialize_all_variables())
+            sess.run(tf.initialize_local_variables())
+            saver = tf.train.Saver()
+            saver.restore(sess, path)
 
-        sess = tf.Session()
-        sess.run(tf.initialize_all_variables())
-        sess.run(tf.initialize_local_variables())
-        saver = tf.train.Saver()
-        saver.restore(sess, path)
-
-        for j in tqdm.tqdm(xrange(total_batch)):
-            content_image, content_name = content_iter.next()
-            print "stylize: " + str(content_name)
-            content_image = np.reshape(content_image, [1, 256, 256, 3]) - mean_pixel
-            output_t = sess.run(output_format, feed_dict={content_holder: content_image})
-            scipy.misc.imsave('coco_style/%s-%s.png' % (content_name[0][5:-4], name), output_t[0])
+            for j in tqdm.tqdm(xrange(total_batch)):
+                content_image, content_name = content_iter.next()
+                print "stylize: " + str(content_name)
+                content_image = np.reshape(content_image, [1, 256, 256, 3]) - mean_pixel
+                output_t = sess.run(output_format, feed_dict={content_holder: content_image})
+                scipy.misc.imsave('coco_style/%s-%s.png' % (content_name[0][5:-4], name), output_t[0])
 
 if __name__ == '__main__':
     # tf.app.run()
