43e09d9219d4995a9d8ff1c1c6f28ea665197819
sdujump <sdujump@gmail.com>
2016-12-13 23:34:52 +0800

    a

--- sequence_data_ori.py
+++ sequence_data_ori.py
@@ -8,7 +8,7 @@ import os
 from tensorflow.python.client import device_lib
 
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 print device_lib.list_local_devices()
 
 
