037521925d2361c8c5438ec5d162a84324791676
sdujump <sdujump@gmail.com>
2016-12-14 14:05:52 +0800

    face

---
+++ infogan_face.py
@@ -0,0 +1,183 @@
+# Import the libraries we will need.
+import tensorflow as tf
+import numpy as np
+import os
+import infogan_faceutil
+import h5py  # for reading our dataset
+from tensorflow.python.client import device_lib
+import tqdm  # making loops prettier
+import scipy
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+print device_lib.list_local_devices()
+
+FLAGS = tf.app.flags.FLAGS
+
+
+hf = h5py.File(''.join(['/home/jump/data/img_align_celeba.h5']), 'r')
+images = hf['images']
+filenames = hf['filenames']
+
+# image, filename = iter_.next()
+
+
+def center_crop(x, crop_h, crop_w=None, resize_w=64):
+    if crop_w is None:
+        crop_w = crop_h
+    h, w = x.shape[1:3]
+    j = int(round((h - crop_h) / 2.))
+    i = int(round((w - crop_w) / 2.))
+    croped = x[:, j:j + crop_h, i:i + crop_w, :]
+    sess = tf.InteractiveSession()
+    tf_resized = tf.image.resize_images(croped, [resize_w, resize_w])
+    resized = tf_resized.eval()
+    sess.close()
+    return resized
+
+
+def show_variables(variales):
+    for i in range(len(variales)):
+        print(variales[i].name)
+
+
+# tf.reset_default_graph()
+batch_size = 100
+z_size = 100  # Size of initial z vector used for generator.
+image_size = 64
+# Define latent variables.
+# Each entry in this list defines a categorical variable of a specific size.
+# categorical_list = 10
+number_continuous = 2  # The number of continous variables.
+num_examples = 12600
+num_epochs = 50  # Total number of iterations to use.
+total_batch = int(np.floor(num_examples / (batch_size)))
+# Directory to save sample images from generator in.
+sample_directory = './figsTut'
+model_directory = './models'  # Directory to save trained model to.
+
+
+def train_infogan():
+    # Create a variable to count the number of train() calls. This equals the
+    # number of batches processed * FLAGS.num_gpus.
+    # global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
+
+    # This initializaer is used to initialize all the weights of the network.
+    # initializer = tf.truncated_normal_initializer(stddev=0.02)
+
+    # These placeholders are used for input into the generator and discriminator, respectively.
+    real_in = tf.placeholder(shape=[None, image_size, image_size, 3], dtype=tf.float32)  # Real images
+    z_lat = tf.placeholder(shape=[None, z_size + number_continuous], dtype=tf.float32)  # Random vector
+
+    # The below code is responsible for applying gradient descent to update
+    # the GAN.
+    trainerD = tf.train.AdamOptimizer(learning_rate=0.0002, beta1=0.5)
+    trainerG = tf.train.AdamOptimizer(learning_rate=0.002, beta1=0.5)
+    trainerQ = tf.train.AdamOptimizer(learning_rate=0.0002, beta1=0.5)
+
+    d_loss, g_loss, q_loss, Gz = tower_loss(real_in, z_lat)
+
+    tvars = tf.trainable_variables()
+    gen_vars = [v for v in tvars if v.name.startswith("generator/")]
+    dis_vars = [v for v in tvars if v.name.startswith("discriminator/")]
+
+    # tf.get_variable_scope().reuse_variables()  # important
+
+    # Only update the weights for the discriminator network.
+    d_grads = trainerD.compute_gradients(d_loss, dis_vars)
+    # Only update the weights for the generator network.
+    g_grads = trainerG.compute_gradients(g_loss, gen_vars)
+    q_grads = trainerG.compute_gradients(q_loss, tvars)
+
+    update_D = trainerD.apply_gradients(d_grads)
+    update_G = trainerG.apply_gradients(g_grads)
+    update_Q = trainerQ.apply_gradients(q_grads)
+
+    saver = tf.train.Saver(tf.all_variables())
+    init = tf.initialize_all_variables()
+    sess = tf.Session()
+    sess.run(init)
+    epoch = 0
+
+    while epoch < num_epochs:
+        iter_ = infogan_faceutil.data_iterator(images, filenames, batch_size)
+        for i in tqdm.tqdm(xrange(total_batch)):
+
+            # Concatenate all c and z variables.
+            zlat = latent_prior(z_size, batch_size)
+
+            # Draw a sample batch from MNIST dataset.
+            image_flat, _ = iter_.next()
+            image_batch = np.reshape(image_flat, [batch_size, 218, 178, 3])
+            image_batch = center_crop(image_batch, crop_h=108)
+            image_batch = (image_batch / 255.0 - 0.5) * 2.0
+
+            _, dLoss = sess.run([update_D, d_loss], feed_dict={real_in: image_batch, z_lat: zlat})  # Update the discriminator
+            # Update the generator, twice for good measure.
+            _, gLoss = sess.run([update_G, g_loss], feed_dict={z_lat: zlat})
+            _, qLoss = sess.run([update_Q, q_loss], feed_dict={z_lat: zlat})  # Update to optimize mutual information.
+
+            if i % 50 == 0:
+                print "epoch: " + str(epoch) + " Gen Loss: " + str(gLoss) + " Disc Loss: " + str(dLoss) + " Q Losses: " + str(qLoss)
+                # Generate another z batch
+                z_sample = np.random.uniform(-1.0, 1.0, size=[batch_size, z_size]).astype(np.float32)
+
+                aa = np.reshape(np.array([[(ee / 4.5 - 1)] for ee in range(10) for tempj in range(10)]), [10, 10]).T
+                bb = np.reshape(aa, [batch_size, 1])
+                cc = np.zeros_like(bb)
+                lcont_sample = np.hstack([bb, cc])
+
+                # Concatenate all c and z variables.
+                zlats = np.concatenate([z_sample, lcont_sample], 1).astype(np.float32)
+                # Use new z to get sample images from generator.
+                samples = sess.run(Gz, feed_dict={z_lat: zlats})
+                if not os.path.exists(sample_directory):
+                    os.makedirs(sample_directory)
+                # Save sample generator images for viewing training
+                # progress.
+                infogan_faceutil.save_images(np.reshape(samples[0:batch_size], [batch_size, image_size, image_size, 3]), [10, 10], sample_directory + '/fig' + str(epoch) + str(i) + '.png')
+        if epoch % 3 == 0:
+            if not os.path.exists(model_directory):
+                os.makedirs(model_directory)
+            saver.save(sess, model_directory + '/model-epoch-' + str(epoch) + '.cptk', global_step=epoch)
+            print "Saved Model"
+        epoch += 1
+
+
+def tower_loss(real_in, z_lat):
+
+    Gz = infogan_faceutil.generator(z_lat)  # Generates images from random z vectors
+    # Produces probabilities for real images
+    Dx, _, = infogan_faceutil.discriminator(real_in)
+    # Produces probabilities for generator images
+    Dg, QgCont1 = infogan_faceutil.discriminator(Gz, reuse=True)
+
+    # These functions together define the optimization objective of the GAN.
+    # This optimizes the discriminator.
+    d_loss = -tf.reduce_mean(tf.log(Dx) + tf.log(1. - Dg), name='dloss')
+    # g_loss = -tf.reduce_mean(tf.log((Dg / (1 - Dg))), name='gloss')  # KL Divergence optimizer
+    g_loss = -tf.reduce_mean(tf.log(Dg), name='gloss')  # KL Divergence optimizer
+    # d_loss = tf.Print(d_loss, [d_loss], 'd_loss = ', summarize=-1)
+
+    # Combine losses for each of the continous variables.
+    q_cont_loss1 = tf.reduce_sum(0.5 * tf.square(z_lat[:, z_size:] - QgCont1), reduction_indices=1)
+    # q_cont_loss2 = tf.reduce_sum(0.5 * tf.square(z_lat[:, z_size + 4:] - QgCont2), reduction_indices=1)
+
+    q_loss = tf.reduce_mean(q_cont_loss1, name='qloss')
+    # q_cont_loss2 = tf.reduce_mean(q_cont_loss2)
+    # q_loss = tf.add(q_cont_loss1, name='qloss')
+
+    return d_loss, g_loss, q_loss, Gz
+
+
+def latent_prior(z_size, batch_size):
+    zs = np.random.uniform(-1.0, 1.0, size=[batch_size, z_size]).astype(np.float32)
+    lcont1 = np.random.uniform(-1, 1, [batch_size, number_continuous])
+    # lcont2 = np.random.uniform(-1, 1, [batch_size, number_continuous])
+    # Concatenate all c and z variables.
+    zlat = np.concatenate([zs, lcont1], 1).astype(np.float32)
+    return zlat
+
+
+if __name__ == '__main__':
+    train_infogan()
+    # test_infogan()
---
+++ infogan_faceutil.py
@@ -0,0 +1,177 @@
+import os
+import numpy as np
+from glob import glob
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import math
+import tqdm  # making loops prettier
+import h5py  # for reading our dataset
+from os import listdir
+from os.path import isfile, join
+import scipy.misc
+import tensorflow.contrib.slim as slim
+
+
+def generator(z, reuse=False):
+    with tf.variable_scope('generator', reuse=reuse):
+
+        initializer = tf.truncated_normal_initializer(stddev=0.02)
+
+        zP = slim.fully_connected(z, 4 * 4 * 512, normalizer_fn=slim.batch_norm,
+                                  activation_fn=tf.nn.relu, scope='g_project', weights_initializer=initializer)
+        zCon = tf.reshape(zP, [-1, 4, 4, 512])
+        gen1 = slim.convolution2d_transpose(
+            zCon, num_outputs=256, kernel_size=[5, 5], stride=2,
+            padding="SAME", normalizer_fn=slim.batch_norm,
+            activation_fn=tf.nn.relu, scope='g_conv1', weights_initializer=initializer)
+
+        gen2 = slim.convolution2d_transpose(
+            gen1, num_outputs=128, kernel_size=[5, 5], stride=2,
+            padding="SAME", normalizer_fn=slim.batch_norm,
+            activation_fn=tf.nn.relu, scope='g_conv2', weights_initializer=initializer)
+
+        gen3 = slim.convolution2d_transpose(
+            gen2, num_outputs=64, kernel_size=[5, 5], stride=2,
+            padding="SAME", normalizer_fn=slim.batch_norm,
+            activation_fn=tf.nn.relu, scope='g_conv3', weights_initializer=initializer)
+        '''
+        gen4 = slim.convolution2d_transpose(
+            gen3, num_outputs=3, kernel_size=[3, 3], stride=2,
+            padding="SAME", normalizer_fn=slim.batch_norm,
+            activation_fn=tf.nn.relu, scope='g_conv4', weights_initializer=initializer)
+        '''
+        g_out = slim.convolution2d_transpose(
+            gen3, num_outputs=3, kernel_size=[5, 5], stride=2,
+            padding="SAME", activation_fn=tf.nn.tanh, scope='g_out', weights_initializer=initializer)
+
+        # g_out_concat = tf.concat(3, [g_out, g_out, g_out], name='concat')
+
+        return g_out
+
+
+def discriminator(bottom, reuse=False):
+    with tf.variable_scope('discriminator', reuse=reuse):
+        initializer = tf.truncated_normal_initializer(stddev=0.02)
+
+        dis1 = slim.convolution2d(bottom, 64, [5, 5], padding="SAME", stride=2, activation_fn=lrelu,
+                                  reuse=reuse, scope='d_conv1', weights_initializer=initializer)
+        # dis1 = tf.space_to_depth(dis1, 2)
+
+        dis2 = slim.convolution2d(dis1, 128, [5, 5], padding="SAME", stride=2,
+                                  normalizer_fn=slim.batch_norm, activation_fn=lrelu,
+                                  reuse=reuse, scope='d_conv2', weights_initializer=initializer)
+        # dis2 = tf.space_to_depth(dis2, 2)
+
+        dis3 = slim.convolution2d(dis2, 256, [5, 5], padding="SAME", stride=2,
+                                  normalizer_fn=slim.batch_norm, activation_fn=lrelu,
+                                  reuse=reuse, scope='d_conv3', weights_initializer=initializer)
+        # dis3 = tf.space_to_depth(dis3, 2)
+
+        dis4 = slim.convolution2d(dis3, 512, [5, 5], padding="SAME", stride=2,
+                                  normalizer_fn=slim.batch_norm, activation_fn=lrelu,
+                                  reuse=reuse, scope='d_conv4', weights_initializer=initializer)
+        '''
+        dis4 = slim.fully_connected(slim.flatten(dis4), 1024, activation_fn=lrelu,
+                                    reuse=reuse, scope='d_fc1', weights_initializer=initializer)
+        '''
+        d_out = slim.fully_connected(slim.flatten(dis4), 1, activation_fn=tf.nn.sigmoid,
+                                     reuse=reuse, scope='d_out', weights_initializer=initializer)
+
+        q_a = slim.fully_connected(slim.flatten(dis4), 128, normalizer_fn=slim.batch_norm,
+                                   reuse=reuse, scope='q_a', weights_initializer=initializer)
+
+        # Here we define the unique layers used for the q-network. The number of outputs depends on the number of
+        # latent variables we choose to define.
+
+        q_cont_outs1 = slim.fully_connected(q_a, 2, activation_fn=tf.nn.tanh, reuse=reuse, scope='q_out_cont1', weights_initializer=initializer)
+        # q_cont_outs2 = slim.fully_connected(q_a, 2, activation_fn=tf.nn.tanh, reuse=reuse, scope='q_out_cont2', weights_initializer=initializer)
+
+        return d_out, q_cont_outs1
+
+
+'''
+def lrelu(x, leak=0.2, name="lrelu"):
+    with tf.variable_scope(name):
+        f1 = 0.5 * (1 + leak)
+        f2 = 0.5 * (1 - leak)
+        return f1 * x + f2 * abs(x)
+'''
+
+
+def lrelu(x, leak=0.2, name="lrelu"):
+    return tf.maximum(x, leak * x)
+
+
+def save_images(images, size, image_path):
+    return imsave(inverse_transform(images), size, image_path)
+
+
+def imsave(images, size, path):
+    return scipy.misc.imsave(path, merge(images, size))
+
+
+def inverse_transform(images):
+    return (images + 1.) / 2.
+
+
+def merge(images, size):
+    h, w = images.shape[1], images.shape[2]
+    img = np.zeros((h * size[0], w * size[1], 3))
+
+    for idx, image in enumerate(images):
+        i = idx % size[1]
+        j = idx / size[1]
+        img[j * h:j * h + h, i * w:i * w + w, :] = image
+
+    return img
+
+
+def data_iterator(images, filenames, batch_size):
+    """ A simple data iterator """
+    batch_idx = 0
+    while True:
+        idxs = np.arange(0, len(images))
+        np.random.shuffle(idxs)
+        for batch_idx in range(0, len(images), batch_size):
+            cur_idxs = idxs[batch_idx:batch_idx + batch_size]
+            cur_idxs = np.sort(cur_idxs)
+            images_batch = images[cur_idxs.tolist()]
+            # images_batch = images_batch.astype("float32")
+            names_batch = filenames[cur_idxs.tolist()]
+            yield images_batch, names_batch
+
+
+def get_dataset(path, dim, channel=3):
+    filenames = [join(path, f) for f in listdir(path) if isfile(
+        join(path, f)) & f.lower().endswith('bmp')]
+    images = np.zeros((len(filenames), dim * dim * channel), dtype=np.uint8)
+    # make a dataset
+    for i in tqdm.tqdm(range(len(filenames))):
+        # for i in tqdm.tqdm(range(10)):
+        image = get_image(filenames[i], dim, dim)
+        images[i] = image.flatten()
+        # get the metadata
+    with h5py.File(''.join(['datasets/dataset-rgb-32.h5']), 'w') as f:
+        images = f.create_dataset("images", data=images)
+        filenames = f.create_dataset('filenames', data=filenames)
+    print("dataset loaded")
+
+
+def get_image(image_path, width, height, mode='RGB'):
+    return scipy.misc.imresize(scipy.misc.imread(image_path, mode=mode), [height, width]).astype(np.float)
+
+
+def gram(layer):
+    shape = tf.shape(layer)
+    num_images = shape[0]
+    num_filters = shape[3]
+    size = tf.size(layer)
+    filters = tf.reshape(layer, tf.pack([num_images, -1, num_filters]))
+    grams = tf.batch_matmul(filters, filters, adj_x=True) / \
+        tf.to_float(size)  # / FLAGS.BATCH_SIZE)
+
+    return grams
+
+
+if __name__ == '__main__':
+    get_dataset('./train_images/', 32)
